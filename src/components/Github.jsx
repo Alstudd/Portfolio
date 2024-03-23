ff8c1e9845e4d45bd9b0d840d11523d6fb48bd9b
Alstudd <alstonsoares17@gmail.com>
2024-03-23 15:28:58 +0530

    styled github contribution calendar

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -1,14 +1,20 @@
 "use client";
 import React, { useState } from "react";
 import GitHubCalendar from "react-github-calendar";
-import { Row } from "react-bootstrap";
 
 const Github = () => {
+
+  const myTheme = {
+    light: ['#f0f0f0', '#c4edde', '#7ac7c4', '#f73859', '#384259'],
+    dark: ['#ffffff','#81d4fa', '#29b6f6', "#039be5", "#0277bd"],
+  };
   const currentYear = new Date().getFullYear();
   const [year, setYear] = useState(currentYear);
   return (
     <div className="relative z-[20] flex flex-col items-center gap-10 justify-center">
       <GitHubCalendar
+      colorScheme="dark"
+        theme={myTheme}
         year={year}
         style={{}}
         username="Alstudd"
@@ -20,14 +26,14 @@ const Github = () => {
       <div className="flex justify-center">
         <button
           disabled={year === new Date("2020").getFullYear()}
-          className="text-center bg-blue-500 hover:bg-blue-600 text-gray-800 font-bold py-2 px-4 rounded-l border-2 border-white"
+          className="text-center text-4xl text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-cyan-400 hover:bg-blue-600 font-bold py-0 px-4 rounded-l border-2 border-white"
           onClick={() => setYear(year - 1)}
         >
           {`<`}
         </button>
         <button
           disabled={year === currentYear}
-          className="text-center bg-blue-500 hover:bg-blue-600 text-gray-800 font-bold py-2 px-4 rounded-r border-2 border-white"
+          className="text-center text-4xl text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-cyan-400 hover:bg-blue-600 font-bold py-0 px-4 rounded-r border-2 border-white"
           onClick={() => setYear(year + 1)}
         >
           {`>`}
