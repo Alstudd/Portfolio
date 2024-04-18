e9356da114533b0934ea54f4bf71ecd53f870c04
Alstudd <alstonsoares17@gmail.com>
2024-04-18 23:46:30 +0530

    infinite slider styling done

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,7 +5,7 @@ import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 
 export default function Skills() {
   return (
-    <div className="mt-20 rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+    <div className="mt-20 rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
       <InfiniteMovingCards
         items={frameworks}
         direction="right"
