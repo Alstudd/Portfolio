55ba714a6dee959e115c572728b1159e3e2ed8e3
Alstudd <alstonsoares17@gmail.com>
2024-05-01 14:36:37 +0530

    Added react awesome reveal animations and framer motion

---
+++ src/components/GithubContributions.tsx
@@ -0,0 +1,40 @@
+import { AttentionSeeker, Bounce, Flip, Zoom } from "react-awesome-reveal";
+import Github from "./Github";
+
+export default function GithubContributions() {
+  return (
+    <section id="github" className="py-8 sm:py-14 flex flex-col gap-14">
+      <Flip direction="vertical" delay={300}>
+        <div className="flex flex-col gap-2 text-center">
+          <h6 className="text-large sm:text-xl md:text-2xl">
+            View my github contributions.
+          </h6>
+          <h3 className="font-semibold text-3xl sm:text-4xl md:text-5xl">
+            Days I{" "}
+            <span className="poppins text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-cyan-400">
+              code on
+            </span>{" "}
+            Github
+          </h3>
+        </div>
+      </Flip>
+      <Flip direction="horizontal" delay={300}>
+        <Github />
+      </Flip>
+      <Zoom direction="down" cascade fraction={0.7}>
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex flex-col text-center">
+            <h3 className="font-semibold text-3xl sm:text-4xl md:text-5xl">
+              Alston&apos;s{" "}
+              <span className="poppins text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-cyan-400">
+                Contribution
+              </span>{" "}
+              Graph
+            </h3>
+          </div>
+          <img src="https://github-readme-activity-graph.vercel.app/graph?username=Alstudd&theme=github-compact&hide_border=true&color=ffffff&line=42a5f5&point=ffffff&hide_title=true" />
+        </div>
+      </Zoom>
+    </section>
+  );
+}
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,12 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Github from "./Github";
 import Contact from "./Contact";
 import InfiniteImages from "./InfiniteImages";
 import InfiniteSlider from "./InfiniteSlider";
 import Hero from "./Hero";
 import MyProjects from "./MyProjects";
 import MySkills from "./MySkills";
+import GithubContributions from "./GithubContributions";
 
 type Props = {};
 
@@ -53,35 +53,7 @@ const Main = (props: Props) => {
         </section>
         <MyProjects />
         <MySkills />
-        <section id="" className="py-8 sm:py-14 flex flex-col gap-14">
-          <div className="flex flex-col gap-2 text-center">
-            <h6 className="text-large sm:text-xl md:text-2xl">
-              View my github contributions.
-            </h6>
-            <h3 className="font-semibold text-3xl sm:text-4xl md:text-5xl">
-              Days I{" "}
-              <span className="poppins text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-cyan-400">
-                code on
-              </span>{" "}
-              Github
-            </h3>
-          </div>
-          <Github />
-        </section>
-        <section id="" className="py-8 sm:py-14 flex flex-col gap-14">
-          <div className="flex flex-col items-center justify-center">
-            <div className="flex flex-col text-center">
-              <h3 className="font-semibold text-3xl sm:text-4xl md:text-5xl">
-                Alston&apos;s{" "}
-                <span className="poppins text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-cyan-400">
-                  Contribution
-                </span>{" "}
-                Graph
-              </h3>
-            </div>
-            <img src="https://github-readme-activity-graph.vercel.app/graph?username=Alstudd&theme=github-compact&hide_border=true&color=ffffff&line=42a5f5&point=ffffff&hide_title=true" />
-          </div>
-        </section>
+        <GithubContributions />
         <section
           id="about"
           className="py-8 sm:py-14 flex flex-col gap-10 relative"
