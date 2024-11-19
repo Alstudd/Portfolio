dfd3b01e5cc625051d7534fcd7aaf3e7c5b3566e
Alstudd <alstonsoares17@gmail.com>
2024-11-20 01:22:56 +0530

    blink change

--- src/app/api/actions/donate/route.ts
+++ src/app/api/actions/donate/route.ts
@@ -1,3 +1,5 @@
+// https://dial.to/devnet?action=solana-action:http://localhost:3000/api/actions/donate
+
 import {
   ActionGetResponse,
   ACTIONS_CORS_HEADERS,
@@ -15,55 +17,42 @@ import {
 } from "@solana/web3.js";
 
 export const GET = async (req: Request) => {
-  try {
-    const payload: ActionGetResponse = {
-      icon: new URL("/alston.jpeg", new URL(req.url).origin).toString(),
-      title: "Alston Soares - Buy me a coffee",
-      label: "Buy me a coffee",
-      description:
-        "If you like my work, consider supporting me by buying me a coffee.",
-      links: {
-        actions: [
-          {
-            href: "/api/actions/donate?amount=0.1",
-            label: "0.1 SOL",
-          },
-          {
-            href: "/api/actions/donate?amount=0.5",
-            label: "0.5 SOL",
-          },
-          {
-            href: "/api/actions/donate?amount=1.0",
-            label: "1.0 SOL",
-          },
-          {
-            href: "/api/actions/donate?amount={amount}",
-            label: "Send SOL",
-            parameters: [
-              {
-                name: "amount",
-                label: "Enter a SOL amount",
-              },
-            ],
-          },
-        ],
-      },
-    };
-
-    return new Response(JSON.stringify(payload), {
-      headers: {
-        "Content-Type": "application/json",
-        ...ACTIONS_CORS_HEADERS,
-      },
-    });
-  } catch (error) {
-    return new Response(JSON.stringify({ error: "GET request failed" }), {
-      headers: {
-        "Content-Type": "application/json",
-        ...ACTIONS_CORS_HEADERS,
-      },
-    });
-  }
+  const payload: ActionGetResponse = {
+    icon: new URL("/alston.jpeg", new URL(req.url).origin).toString(),
+    title: "Alston Soares - Buy me a coffee",
+    label: "Buy me a coffee",
+    description:
+      "If you like my work, consider supporting me by buying me a coffee.",
+    links: {
+      actions: [
+        {
+          href: "/api/actions/donate?amount=0.1",
+          label: "0.1 SOL",
+        },
+        {
+          href: "/api/actions/donate?amount=0.5",
+          label: "0.5 SOL",
+        },
+        {
+          href: "/api/actions/donate?amount=1.0",
+          label: "1.0 SOL",
+        },
+        {
+          href: "/api/actions/donate?amount={amount}",
+          label: "Send SOL",
+          parameters: [
+            {
+              name: "amount",
+              label: "Enter a SOL amount",
+            },
+          ],
+        },
+      ],
+    },
+  };
+  return new Response(JSON.stringify(payload), {
+    headers: ACTIONS_CORS_HEADERS,
+  });
 };
 
 export const OPTIONS = GET;
@@ -72,14 +61,26 @@ export const POST = async (req: Request) => {
   try {
     const url = new URL(req.url);
     const body: ActionPostRequest = await req.json();
-    const account = new PublicKey(body.account);
-    const amount = parseFloat(url.searchParams.get("amount") || "0.1") || 0.1;
+    let account: PublicKey;
+    try {
+      account = new PublicKey(body.account);
+    } catch (err) {
+      throw "Invalid account";
+    }
 
-    const connection = new Connection(clusterApiUrl("devnet"));
+    let amount: number = 0.1;
+    if (url.searchParams.has("amount")) {
+      try {
+        amount = parseFloat(url.searchParams.get("amount") || "0.1") || amount;
+      } catch (err) {
+        throw "Invalid 'amount' input";
+      }
+    }
+
+    const connection = new Connection(clusterApiUrl("mainnet-beta"));
     const TO_PUBKEY = new PublicKey(
       `${process.env.NEXT_PUBLIC_SOLANA_PUBLIC_KEY}`
     );
-
     const transaction = new Transaction().add(
       SystemProgram.transfer({
         fromPubkey: account,
@@ -87,7 +88,6 @@ export const POST = async (req: Request) => {
         toPubkey: TO_PUBKEY,
       })
     );
-
     transaction.feePayer = account;
     transaction.recentBlockhash = (
       await connection.getLatestBlockhash()
@@ -99,20 +99,16 @@ export const POST = async (req: Request) => {
         message: "Thank you for the coffee!",
       },
     });
-
     return new Response(JSON.stringify(payload), {
-      headers: {
-        "Content-Type": "application/json",
-        ...ACTIONS_CORS_HEADERS,
-      },
+      headers: ACTIONS_CORS_HEADERS,
     });
   } catch (error) {
-    const message = typeof error === "string" ? error : "An error occurred";
-    return new Response(JSON.stringify({ error: message }), {
-      headers: {
-        "Content-Type": "application/json",
-        ...ACTIONS_CORS_HEADERS,
-      },
+    let message = "An error occurred";
+    if (typeof error === "string") {
+      message = error;
+    }
+    return new Response(JSON.stringify(message), {
+      headers: ACTIONS_CORS_HEADERS,
     });
   }
 };
